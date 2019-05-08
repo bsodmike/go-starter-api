d8ba46fda8969e1fa6b2248a2f3a71451da9b997
Michael de Silva <michael@inertialbox.com>
2019-05-08 16:51:58 +0530

    Add db.go

---
+++ models/db.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	gorm "github.com/jinzhu/gorm"
+	// postgress db driver
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+// DB abstraction
+type DB struct {
+	*gorm.DB
+}
+
+// NewPostgresDB - postgres database
+func NewPostgresDB(dataSourceName string) *DB {
+
+	db, err := gorm.Open("postgres", dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.DB().Ping(); err != nil {
+		panic(err)
+	}
+
+	//db.LogMode(true)
+
+	return &DB{db}
+}
